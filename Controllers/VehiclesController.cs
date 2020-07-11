5791c9fa1da7b82d661ba76c0f9d0b027e54e9b4
i7HoneyBadger <grygorii.diachenko@ukr.net>
2020-07-11 18:25:55 +0300

    Implemented Server-side Validation for Vehicle.

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -22,6 +22,23 @@ namespace vega.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // Business rule validation
+            // if(true)
+            // {
+            //     ModelState.AddModelError("...", "error");
+            //     return BadRequest(ModelState);
+            // }
+
+            // var model = await context.Models.FindAsync(vehicleResource.ModelId);
+            // if(model == null)
+            // {
+            //     ModelState.AddModelError("ModelId", "Invalid modelId.");
+            //     return BadRequest(ModelState);
+            // }
+
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
             context.Vehicles.Add(vehicle);
--- Persistance/VegaDbContext.cs
+++ Persistance/VegaDbContext.cs
@@ -7,6 +7,7 @@ namespace vega.Persistance
     {
         public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
         public DbSet<Feature> Features { get; set; }
        /*  public VegaDbContext(string connectionString)
             :base(connectionString)
