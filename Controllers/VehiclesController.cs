243a074ccd0ee6cb53fa4d0916f9b4aeaad9d16d
i7HoneyBadger <grygorii.diachenko@ukr.net>
2020-07-11 16:06:46 +0300

    VehicleController plus Vehicle and Contact DTOs.

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using vega.Models;
+
+namespace vega.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        [HttpPost]
+        public IActionResult CreateVehicle([FromBody] Vehicle vehicle){
+            return Ok(vehicle);
+        }
+    }
+}
