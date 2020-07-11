c9b8ad42d58575e4957fd750c8de9f64ac5bf4c0
i7HoneyBadger <grygorii.diachenko@ukr.net>
2020-07-11 20:33:15 +0300

    Imperemented HttpDelete and HttpGet in Vehicles.

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -56,6 +56,10 @@ namespace vega.Controllers
                 return BadRequest(ModelState);
 
             var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+           
+            if(vehicle == null)
+                return NotFound();
+
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
 
@@ -64,6 +68,31 @@ namespace vega.Controllers
             var result = mapper.Map<Vehicle,VehicleResource>(vehicle);
 
             return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id){
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if(vehicle == null)
+                return NotFound();
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id){
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if(vehicle == null)
+                return NotFound();
+
+            var vehicleResource = mapper.Map<Vehicle,VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
         }
     }
 }
