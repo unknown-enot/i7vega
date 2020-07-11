b5778da575891f59ee89fa5c6b6c98821ce77a38
i7HoneyBadger <grygorii.diachenko@ukr.net>
2020-07-11 20:20:08 +0300

    Refactored collections mapping using LINQ.

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -25,18 +25,37 @@ namespace vega.Mapping
                 .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
                 .ForMember(v => v.Features, opt => opt.Ignore())
                 .AfterMap((vr, v) => {
-                    // Remove unselected features
-                    var removedFeatures = new List<VehicleFeature>();
-                    foreach(var f in v.Features)
-                        if(!vr.Features.Contains(f.FeatureId))
-                            removedFeatures.Add(f);
-                    foreach(var f in removedFeatures)
+                    // Remove unselected features. Way1.
+                    //var removedFeatures = new List<VehicleFeature>();
+                    //foreach(var f in v.Features)
+                    //    if(!vr.Features.Contains(f.FeatureId))
+                    //        removedFeatures.Add(f);
+
+                    // Remove unselected features. LINQ Way.
+                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId));
+                    foreach(var f in removedFeatures.ToList())
                         v.Features.Remove(f);
 
-                    // Add new features
-                    foreach(var id in vr.Features)
-                        if(!v.Features.Any(f => f.FeatureId == id))
-                            v.Features.Add(new VehicleFeature { FeatureId = id});
+                    /*
+                        -> Collection was modified; enumeration operation may not execute -< 
+                        What's likely happening is that removedFeatures is indirectly changing the removedFeatures 
+                        under the hood during the loop and leading to that message. You can verify this by changing
+                        foreach(var f in removedFeatures.ToList()).
+                        That copies the values of removedFeatures to a separate list at the start of the foreach. 
+                        Nothing else has access to this list (it doesn't even have a variable name!), so nothing can modify it inside the loop.
+                    */    
+
+                    // Add new features. Way1
+                    // foreach(var id in vr.Features)
+                    //     if(!v.Features.Any(f => f.FeatureId == id))
+                    //         v.Features.Add(new VehicleFeature { FeatureId = id});
+
+
+                    // Add new features. LINQ Way.
+                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature { FeatureId = id });
+                    foreach(var f in addedFeatures)
+                        v.Features.Add(f);
+                
                 });
         }
     }
