4f07b3dc8c68dc2f4ff9b0f14b151b90eb9e37b8
i7HoneyBadger <grygorii.diachenko@ukr.net>
2020-07-12 16:27:21 +0300

    Decoupled from EF Get, Add, Remove actions.

--- Persistance/IVehicleRepository.cs
+++ Persistance/IVehicleRepository.cs
@@ -5,6 +5,9 @@ namespace vega.Persistance
 {
     public interface IVehicleRepository
     {
-        Task<Vehicle> GetVehicle(int id);
+        Task<Vehicle> GetVehicle(int id, bool includeRelated = true);
+        void Add(Vehicle vehicle);
+        void Remove(Vehicle vehicle);
+        
     }
 }
