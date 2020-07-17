c87fc19bd2f5f4184020930e449c677278a63ef1
i7HoneyBadger <grygorii.diachenko@ukr.net>
2020-07-17 12:33:51 +0300

    Fixed problem with Zone in Angular for Toastr.

--- ClientApp/src/app/app.error-handler.ts
+++ ClientApp/src/app/app.error-handler.ts
@@ -1,13 +1,17 @@
-import { ErrorHandler, Inject } from "@angular/core";
+import { ErrorHandler, Inject, NgZone } from "@angular/core";
 import { ToastrService } from "ngx-toastr";
 
 export class AppErrorHandler implements ErrorHandler {
-    constructor(@Inject(ToastrService) private toastrService: ToastrService){
+    constructor(
+        private ngZone: NgZone, 
+        @Inject(ToastrService) private toastrService: ToastrService){
 
     }
 
     handleError(error: any) :void {
-        this.toastrService.error('An unexpected error happened', 'Error');
-
+        this.ngZone.run(() => {
+            this.toastrService.error('An unexpected error happened', 'Error');
+        });
+        
     }
 }
