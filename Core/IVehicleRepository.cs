8c48ca957221d7902c49da72b53adc1334a1e28a
i7HoneyBadger <grygorii.diachenko@ukr.net>
2020-07-18 14:32:10 +0300

    Filtering on the Server on MakeId.

--- Core/IVehicleRepository.cs
+++ Core/IVehicleRepository.cs
@@ -11,6 +11,6 @@ namespace vega.Core
         Task<Vehicle> GetVehicleWithFeatures(int id);
         void Add(Vehicle vehicle);
         void Remove(Vehicle vehicle);
-        Task<IEnumerable<Vehicle>> GetVehicles();
+        Task<IEnumerable<Vehicle>> GetVehicles(Filter filter);
     }
 }
