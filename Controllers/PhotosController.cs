c865ada3c0561b4d59a749d735549d72e44043d1
i7HoneyBadger <grygorii.diachenko@ukr.net>
2020-07-20 17:39:46 +0300

    A few corrections.

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -11,53 +11,46 @@ using vega.Core.Models;
 
 namespace vega.Controllers
 {
-    // /api/vehicles/1/photos
-    // /api/vehicles/1/photos/1
-    [Route("/api/vehicles/{vehicleId}/photos")]
-    public class PhotosController : Controller
+  [Route("/api/vehicles/{vehicleId}/photos")]
+  public class PhotosController : Controller
+  {
+    private readonly IWebHostEnvironment host;
+    private readonly IVehicleRepository repository;
+    private readonly IUnitOfWork unitOfWork;
+    private readonly IMapper mapper;
+    public PhotosController(IWebHostEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
     {
-        private readonly IWebHostEnvironment host;
-        private readonly IVehicleRepository repository;
-        private readonly IUnitOfWork unitOfWork;
-        private readonly IMapper mapper;
-        public PhotosController(IWebHostEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
-        {
-            this.mapper = mapper;
-            this.unitOfWork = unitOfWork;
-            this.repository = repository;
-            this.host = host;
-            //host.WebRootPath  //wwwroot
-
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
-        {
-            var vehicle = await repository.GetVehicle(vehicleId, includeRelated: false);
-            if (vehicle == null)
-                return NotFound();
-
-            var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
-            if (!Directory.Exists(uploadsFolderPath))
-                Directory.CreateDirectory(uploadsFolderPath);
-
-            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-            var filePath = Path.Combine(uploadsFolderPath, fileName);
-
-            using (var stream = new FileStream(filePath, FileMode.Create))
-            {
-                await file.CopyToAsync(stream);
-            }
-
-            // System.Drawing  to create a thumbnail
-
-            // Update the database
-            var photo = new Photo { FileName = fileName };
-            vehicle.Photos.Add(photo);
-
-            await unitOfWork.CompleteAsync();
-
-            return Ok(mapper.Map<Photo,PhotoResource>(photo));
-        }
+      this.mapper = mapper;
+      this.unitOfWork = unitOfWork;
+      this.repository = repository;
+      this.host = host;
     }
+    [HttpPost]
+    public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
+    {
+        var vehicle = await repository.GetVehicle(vehicleId, includeRelated: false);
+        if (vehicle == null)
+            return NotFound();
+
+        if (file == null) return BadRequest();
+
+        var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
+        if (!Directory.Exists(uploadsFolderPath))
+            Directory.CreateDirectory(uploadsFolderPath);
+      
+        var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+        var filePath = Path.Combine(uploadsFolderPath, fileName);
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        var photo = new Photo { FileName = fileName };
+        vehicle.Photos.Add(photo);
+        await unitOfWork.CompleteAsync();
+
+        return Ok(mapper.Map<Photo, PhotoResource>(photo));
+    }
+  }
 }
